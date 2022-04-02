42e84102e88ee24d2c683aa206bc3daa69262b3f
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2022-04-02 16:22:15 -0700

    added connection to mongoDb

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import * as dotenv from "dotenv";
-import cors from cors;
+import cors from 'cors';
 
 dotenv.config();
-
 const app = express();
-
 app.use(cors());
 
-const port = process.env.PORT || 8080;
+mongoose.connect(
+  process.env.DATABASE_URI, 
+  {useNewUrlParser: true, useUnifiedTopology: true}
+)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
+const port = process.env.PORT || 8080;
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`),
 );
