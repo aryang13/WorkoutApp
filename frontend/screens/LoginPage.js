8ef873c057f52a018bc24d41c53fac21a730be15
straw-hat-gupta <strawhatgupta@gmail.com>
2022-09-02 18:16:56 -0700

    saving state login

--- frontend/screens/LoginPage.js
+++ frontend/screens/LoginPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, View, Text, TextInput, Alert } from 'react-native';
 import LoginStyleSheet from '../styles/LoginStyleSheet';
 
@@ -7,28 +7,40 @@ export default function LoginPage({ navigation }) {
         navigation.navigate('SignUpPage')
     }
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function validateUserInfo(username, password) {
+        console.log(username);
+        console.log(password);
+    }
+
     return (
         <View style={LoginStyleSheet.container}>
             <View style={LoginStyleSheet.margin}>
                 <Text style={LoginStyleSheet.setFontSizeOne}>Login</Text>
             </View>
+
             <View style={LoginStyleSheet.margin}>
                 <TextInput
                     style={LoginStyleSheet.textBox}
                     placeholder='Username'
+                    onChangeText={(val) => setUsername(val)}
                 />
             </View>
+            
             <View style={LoginStyleSheet.margin}>
                 <TextInput
                     placeholder='Password'
                     style={LoginStyleSheet.textBox}
                     secureTextEntry={true}
+                    onChangeText={(val) => setPassword(val)}
                 />
             </View>
             <View styles={LoginStyleSheet.margin}>
                 <Button
                     title='Login'
-                    onPress={() => Alert.alert("Not yet hoe")}
+                    onPress={validateUserInfo(username, password)}
                 />
                 <Button
                     title='Sign Up'
